734500c0513b3ee65ca116d3e0c7f711b00ede6a
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-05-30 19:57:32 +0000

    removi minhas perguntas do html para poder colocá-las no java script

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'FUTEBOL',
+    'Qual é o tempo de duração de uma partida de futebol?',
+    'Uma partida de futebol tem 90 minutos de duração, divididos em dois tempos de 45 minutos cada.'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quem é considerado o melhor jogador de todos os tempos?',
+    'O jogador considerado o melhor de todos os tempos é Pelé.'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'O que é a Copa do Mundo?',
+    'A Copa do Mundo é uma competição de futebol realizada pela Federação Internacional de Futebol a cada quatro anos.'
+)
