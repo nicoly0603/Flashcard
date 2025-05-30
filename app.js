a8ca1a0dbb5de77e9e57193f9f99d8f5649fa7cd
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-05-30 21:26:01 +0000

    adicionei varias perguntas nos cartões

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let container = document.getElementById('container')
+    let cartão = document.createElement('article')
+    cartão.className = 'cartão'
+
+    cartão.innerHTML = `
+    <div class="cartão__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartão__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class=cartão__conteudo__resposta>
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    container.appendChild(cartão)
+    
 }
--- perguntas.js
+++ perguntas.js
@@ -1,4 +1,16 @@
 criaCartao(
+    'ATLETISMO',
+    'O que é arremesso de peso?',
+    'Arremesso de peso é uma modalidade de atletismo em que o atleta arremessa uma bola de 1kg a 5kg na maior distância possível.'
+)
+
+criaCartao(
+    'ATLETISMO',
+    'O que é corrida de revezamento?',
+    'A corrida de revezamento é uma prova do atletismo realizada em equipes com quatro corredores. Os atletas devem revezar entre si no transporte de um bastão durante o percurso de corrida.'
+)
+
+criaCartao(
     'FUTEBOL',
     'Qual é o tempo de duração de uma partida de futebol?',
     'Uma partida de futebol tem 90 minutos de duração, divididos em dois tempos de 45 minutos cada.'
@@ -15,3 +27,22 @@ criaCartao(
     'O que é a Copa do Mundo?',
     'A Copa do Mundo é uma competição de futebol realizada pela Federação Internacional de Futebol a cada quatro anos.'
 )
+
+criaCartao(
+    'BASQUETE',
+    'O que é um rebote?',
+    'Recuperar a bola após um arremesso errado (que a bola bateu na tabela ou aro, ou seja, não entrou na cesta).'
+)
+
+criaCartao(
+    'BASQUETE',
+    'O que é NBA?',
+    '"National Baketball Assossiation"(NBA) é a principal liga profissional de basquete do mundo.'
+)
+
+criaCartao(
+    'BASQUETE',
+    'Quem é considerado o maior jogador da história do basquete?',
+    'Michael Jordan.'
+)
+
