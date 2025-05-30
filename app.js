00b00cc35b20454f8f2ee3902bca8de3f8b12538
nicoly0603 <santos.nicoly06@escola.pr.gov.br>
2025-05-30 21:42:24 +0000

    adicionando click para virar os cartoes

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
-    container.appendChild(cartão)
     
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    
+
+
+    container.appendChild(cartão)
+
 }
